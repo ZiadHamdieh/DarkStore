f6642e57c7e5e8a93e4a4888d9d4a094065943a6
Ziad Hamdieh <z.a.hamdieh@gmail.com>
2019-03-13 23:53:45 -0400

    added tabBarController

---
+++ AppStore/Controllers/TabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  TabBarController.swift
+//  AppStore
+//
+//  Created by Ziad Hamdieh on 2019-03-13.
+//  Copyright © 2019 Ziad Hamdieh. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        viewControllers = [
+            setupNavController(forViewController: UIViewController(), withtitle: "Today", imageName: "today"),
+            setupNavController(forViewController: UIViewController(), withtitle: "Apps", imageName: "apps"),
+            setupNavController(forViewController: UIViewController(), withtitle: "Search", imageName: "search")
+        ]
+    }
+    
+    fileprivate func setupNavController(forViewController viewController: UIViewController, withtitle title: String, imageName: String) -> UIViewController {
+        
+        let newNavController = UINavigationController(rootViewController: viewController)
+        newNavController.tabBarItem.title = title
+        newNavController.navigationItem.title = title
+        newNavController.tabBarItem.image = UIImage(named: imageName)
+        newNavController.navigationBar.prefersLargeTitles = true
+        
+        viewController.view.backgroundColor = .white
+        viewController.navigationItem.title = title
+        
+        return newNavController
+    }
+}
